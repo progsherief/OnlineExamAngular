cf565e83f19c62c64bd5cf9b8b5b79e8b505df99
progsherief <progsherief@gmail.com>
2025-07-31 16:51:21 +0300

    add bootstrap

--- OnlineExamAngular/src/app/app.routes.ts
+++ OnlineExamAngular/src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {
+        path:"",
+        // component:LoginComponent,
+        loadComponent:()=>import("./features/auth/pages/login/login.component")
+        .then(c=>c.LoginComponent)
+
+    },
+    {
+        
+          path:"register",
+        // component:LoginComponent,
+        loadComponent:()=>import("./features/auth/pages/register/register.component")
+        .then(c=>c.RegisterComponent)
+    }
+];
